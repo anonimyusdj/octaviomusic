e056bf26c17a9d5d464fd39838e1e7976e6fa1e9
anonimyusdj <anonimyusdj@gmail.com>
2018-10-04 02:38:40 -0600

    index

--- controllers/index.controller.php
+++ controllers/index.controller.php
@@ -3,6 +3,8 @@
 class IndexController{
     
     public function index(){
+        require_once "view/partes/header.php";
         require_once "view/home/home.php";
+        require_once "view/partes/footer.php";
     }
 }
---
+++ view/partes/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="Expires" content="0">
+    <meta http-equiv="Last-Modified" content="0">
+    <meta http-equiv="Cache-Control" content="no-cache, mustrevalidate">
+    <meta http-equiv="Pragma" content="no-cache">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Musica Octavio</title>
+</head>
+<body>
+    <script src="assets/js/require.js" data-main="assets/js/home"></script>
