959521b7d6bd1821d175e400cb974aeaddc85b3d
anonimyusdj <anonimyusdj@gmail.com>
2018-10-04 12:43:33 -0600

    css js

--- view/partes/header.php
+++ view/partes/header.php
@@ -14,10 +14,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?php echo $title;?></title>
-    <link rel="stylesheet" href="assets/css/materialize.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.7.4/angular.min.js"></script>
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
+    <script src="assets/js/css.js"></script>    
 </head>
 <body>
-    <script src="assets/js/require.js" data-main="assets/js/home"></script>
-    <script src="assets/js/css.js"></script>
+
