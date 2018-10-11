d7422e3a7cd92f8e41c86f4158776bb6e2612b08
anonimyusdj <anonimyusdj@gmail.com>
2018-10-11 17:49:02 -0600

    menu

--- view/home/menu.php
+++ view/home/menu.php
@@ -4,11 +4,11 @@
       <a href="#!" class="brand-logo"><?php echo $autor ?></a>
       <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
       <ul class="right hide-on-med-and-down">
-         <li><a style=" font-size:20px; font-weight: bold;" href="index.php"><i class="material-icons left">home</i>Inicio</a></li>
-        <li><a style=" font-size:20px; font-weight: bold;" href="sass.html"><i class="material-icons left">panorama</i>Multimedias</a></li>
-        <li><a style=" font-size:20px; font-weight: bold;" href="badges.html"><i class="material-icons left">art_track</i>Eventos</a></li>
-        <li><a style=" font-size:20px; font-weight: bold;" href="#!/music"><i class="material-icons left">headset</i>Música</a></li>
-        <li><a style=" font-size:20px; font-weight: bold;" href="mobile.html"><i class="material-icons  left">face</i>Biografía</a></li>
+         <li><a style=" font-size:20px;" href="index.php"><i class="material-icons left">home</i>Inicio</a></li>
+        <li><a style=" font-size:20px;" href="sass.html"><i class="material-icons left">panorama</i>Multimedias</a></li>
+        <li><a style=" font-size:20px;" href="badges.html"><i class="material-icons left">art_track</i>Eventos</a></li>
+        <li><a style=" font-size:20px;" href="#!/music"><i class="material-icons left">headset</i>Música</a></li>
+        <li><a style=" font-size:20px;" href="mobile.html"><i class="material-icons  left">face</i>Biografía</a></li>
       </ul>
     </div>
   </nav>
