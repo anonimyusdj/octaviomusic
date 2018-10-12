c4b45b47903b1363629d00d9952f0b236ab1772a
anonimyusdj <anonimyusdj@gmail.com>
2018-10-11 21:39:12 -0600

    reproductor

--- controllers/index.controller.php
+++ controllers/index.controller.php
@@ -8,4 +8,10 @@ class IndexController{
         require_once "view/home/home.php";
         require_once "view/partes/footer.php";
     }
+    public function musica(){
+        require_once "view/partes/header.php";
+        require_once "view/home/menu.php";
+        require_once "view/home/reproductor.php";
+        require_once "view/partes/footer.php";
+    }
 }
--- view/home/menu.php
+++ view/home/menu.php
@@ -7,14 +7,14 @@
         <li><a style=" font-size:20px;" href="index.php"><i class="material-icons left">home</i>Inicio</a></li>
         <li><a style=" font-size:20px;" href="sass.html"><i class="material-icons left">panorama</i>Multimedias</a></li>
         <li><a style=" font-size:20px;" href="badges.html"><i class="material-icons left">art_track</i>Eventos</a></li>
-        <li><a style=" font-size:20px;" href="#!/music"><i class="material-icons left">headset</i>Música</a></li>
+        <li><a style=" font-size:20px;" href="?c=index&a=musica"><i class="material-icons left">headset</i>Música</a></li>
         <li><a style=" font-size:20px;" href="mobile.html"><i class="material-icons  left">face</i>Biografía</a></li>
       </ul>
       <ul class="side-nav" id="mobile-demo">
         <li><a style=" font-size:20px; font-weight: bold;" href="index.php"><i class="material-icons left">home</i>Inicio</a></li>
         <li><a style=" font-size:20px; font-weight: bold;" href="sass.html"><i class="material-icons left">panorama</i>Multimedias</a></li>
         <li><a style=" font-size:20px; font-weight: bold;" href="badges.html"><i class="material-icons left">art_track</i>Eventos</a></li>
-        <li><a style=" font-size:20px; font-weight: bold;" href="#!/music"><i class="material-icons left">headset</i>Música</a></li>
+        <li><a style=" font-size:20px; font-weight: bold;" href="?c=index&a=musica"><i class="material-icons left">headset</i>Música</a></li>
         <li><a style=" font-size:20px; font-weight: bold;" href="mobile.html"><i class="material-icons  left">face</i>Biografía</a></li>
       </ul>
     </div>
