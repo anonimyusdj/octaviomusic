941327c96dadf7608031fdb810240d00b301fc5f
anonimyusdj <anonimyusdj@gmail.com>
2018-10-11 21:59:33 -0600

    reproductor

--- view/home/reproductor.php
+++ view/home/reproductor.php
@@ -6,31 +6,41 @@
     <div class="col s12 m1"></div>
 
     <div class="col s12 m4">
-        <div id="player">
-            <img src="" class="cover" id="art" />
-            <div class="trackinfo">
-                <p id="title"></p>
-                <p id="artist"></p>
+        <div class="containerPlayer">
+
+            <div id="listContainer" class="playlistContainer">
+            <ul id="playListContainer">
+                <li data-src="http://abarcarodriguez.com/365/files/offspring.mp3">
+                <a href="#">Adele - Rolling In The Deep</a>
+                </li>
+                <li data-src="http://abarcarodriguez.com/365/files/anamanaguchi.mp3">
+                <a href="#">Bruno - When I Was Your Man</a>
+                </li>
+                </ul>
             </div>
-            <div id="track">
-                <div id="progress"></div>
-                <div id="handler"></div>
-            </div>
-            <div class="container">
-                <div class="info">
-                    <div class="cont">
-                        <button id="prev" class="nav">
-                            <img src="http://abarcarodriguez.com/lab/prev.png" />
-                        </button>
-                        <button id="play" class="nav">
-                            <img class="pad" src="http://abarcarodriguez.com/lab/play.png" />
-                        </button>
-                        <button id="next" class="nav">
-                            <img src="http://abarcarodriguez.com/lab/next.png" />
-                        </button>
-                    </div>
+
+            <div id="playerContainer">
+                <ul class="controls">
+                    <li><a href="#" class="shuffle shuffleActive" data-attr="shuffled"></a></li>
+                    <li><a href="#" class="left" data-attr="prevAudio"></a></li>
+                    <li><a href="#" class="play" data-attr="playPauseAudio"></a></li>
+                    <li><a href="#" class="right" data-attr="nextAudio"></a></li>
+                    <li><a href="#" class="repeat" data-attr="repeatSong"></a></li>
+                </ul>
+                <div class="audioDetails">
+                    <span class="songPlay"></span>
+                    <span data-attr="timer" class="audioTime"></span>
+                </div>
+                <div class="progress">
+                    <div data-attr="seekableTrack" class="seekableTrack"></div>
+                    <div class="updateProgress"></div>
+                </div>
+                <div class="volumeControl">
+                    <div class="volume volume1"></div>
+                    <input class="bar" data-attr="rangeVolume" type="range" min="0" max="1" step="0.1" value="0.7" />
                 </div>
             </div>
+
         </div>
     </div>
 
