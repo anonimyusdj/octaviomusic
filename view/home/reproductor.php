08d512f19763aa9b8140ad3cf738ec6c3366dcf6
anonimyusdj <anonimyusdj@gmail.com>
2018-10-11 21:46:57 -0600

    reproductor

--- view/home/reproductor.php
+++ view/home/reproductor.php
@@ -1,26 +1,42 @@
-<div id="player">
-    <img src="" class="cover" id="art" />
-    <div class="trackinfo">
-        <p id="title"></p>
-        <p id="artist"></p>
+<div class="row">
+    <div class="col s12 m3">
+
     </div>
-    <div id="track">
-        <div id="progress"></div>
-        <div id="handler"></div>
-    </div>
-    <div class="container">
-        <div class="info">
-            <div class="cont">
-                <button id="prev" class="nav">
-                    <img src="http://abarcarodriguez.com/lab/prev.png" />
-                </button>
-                <button id="play" class="nav">
-                    <img class="pad" src="http://abarcarodriguez.com/lab/play.png" />
-                </button>
-                <button id="next" class="nav">
-                    <img src="http://abarcarodriguez.com/lab/next.png" />
-                </button>
+
+    <div class="col s12 m1"></div>
+
+    <div class="col s12 m4">
+        <div id="player">
+            <img src="" class="cover" id="art" />
+            <div class="trackinfo">
+                <p id="title"></p>
+                <p id="artist"></p>
+            </div>
+            <div id="track">
+                <div id="progress"></div>
+                <div id="handler"></div>
+            </div>
+            <div class="container">
+                <div class="info">
+                    <div class="cont">
+                        <button id="prev" class="nav">
+                            <img src="http://abarcarodriguez.com/lab/prev.png" />
+                        </button>
+                        <button id="play" class="nav">
+                            <img class="pad" src="http://abarcarodriguez.com/lab/play.png" />
+                        </button>
+                        <button id="next" class="nav">
+                            <img src="http://abarcarodriguez.com/lab/next.png" />
+                        </button>
+                    </div>
+                </div>
             </div>
         </div>
+    </div>
+
+    <div class="col s12 m1"></div>
+
+    <div class="col s12 m3">
+    
     </div>
 </div>
