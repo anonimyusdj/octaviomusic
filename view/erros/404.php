6970dc9d3e8d546d56026ad18311280557faa457
anonimyusdj <anonimyusdj@gmail.com>
2018-10-13 20:50:24 -0600

    error

--- view/erros/404.php
+++ view/erros/404.php
@@ -1,2 +1,3 @@
+<a class="waves-effect waves-light btn-large <?php echo $color; ?> darken-1"><i class="material-icons right">home</i>Regresar al Home</a>
 <img class="materialboxed" width="650" src="<?php echo $imagenes;?>error4041.png">
 <img class="materialboxed" width="650" src="<?php echo $imagenes;?>error4042.png">
