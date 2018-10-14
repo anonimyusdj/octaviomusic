55db28e7105911eaa1e7c7dd60c32645f4f4e16a
anonimyusdj <anonimyusdj@gmail.com>
2018-10-13 20:31:16 -0600

    error404

--- controllers/index.controller.php
+++ controllers/index.controller.php
@@ -14,4 +14,10 @@ class IndexController{
         require_once "view/home/reproductor.php";
         require_once "view/partes/footer.php";
     }
+    public function error404(){
+        require_once "view/partes/header.php";
+        require_once "view/home/menu.php";
+        require_once "view/errors/404.php";
+        require_once "view/partes/footer.php";
+    }
 }
---
+++ view/erros/404.php
@@ -0,0 +1,2 @@
+<img class="materialboxed" width="650" src="<?php echo $imagenes;?>error4041.png">
+<img class="materialboxed" width="650" src="<?php echo $imagenes;?>error4042.png">
