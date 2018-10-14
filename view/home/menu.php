cca1f340656b489f7d99eff600ba5598c027d623
anonimyusdj <anonimyusdj@gmail.com>
2018-10-13 20:56:33 -0600

    menu

--- view/home/menu.php
+++ view/home/menu.php
@@ -1,21 +1,18 @@
-
-<nav>
-    <div class="nav-wrapper <?php echo $color; ?> darken-3">
-      <a href="#!" class="brand-logo"><img src="<?php echo $imagenes;?>logo.jpg" alt=""  width="45px"><?php echo $autor ?></a>
+  <nav>
+    <div class="nav-wrapper">
+      <a href="#!" class="brand-logo">Logo</a>
       <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
       <ul class="right hide-on-med-and-down">
-        <li><a style=" font-size:20px;" href="index.php"><i class="material-icons left">home</i>Inicio</a></li>
-        <li><a style=" font-size:20px;" href="sass.html"><i class="material-icons left">panorama</i>Multimedias</a></li>
-        <li><a style=" font-size:20px;" href="badges.html"><i class="material-icons left">art_track</i>Eventos</a></li>
-        <li><a style=" font-size:20px;" href="?c=index&a=musica"><i class="material-icons left">headset</i>Música</a></li>
-        <li><a style=" font-size:20px;" href="mobile.html"><i class="material-icons  left">face</i>Biografía</a></li>
+        <li><a href="sass.html">Sass</a></li>
+        <li><a href="badges.html">Components</a></li>
+        <li><a href="collapsible.html">Javascript</a></li>
+        <li><a href="mobile.html">Mobile</a></li>
       </ul>
       <ul class="side-nav" id="mobile-demo">
-        <li><a style=" font-size:20px; font-weight: bold;" href="index.php"><i class="material-icons left">home</i>Inicio</a></li>
-        <li><a style=" font-size:20px; font-weight: bold;" href="sass.html"><i class="material-icons left">panorama</i>Multimedias</a></li>
-        <li><a style=" font-size:20px; font-weight: bold;" href="badges.html"><i class="material-icons left">art_track</i>Eventos</a></li>
-        <li><a style=" font-size:20px; font-weight: bold;" href="?c=index&a=musica"><i class="material-icons left">headset</i>Música</a></li>
-        <li><a style=" font-size:20px; font-weight: bold;" href="mobile.html"><i class="material-icons  left">face</i>Biografía</a></li>
+        <li><a href="sass.html">Sass</a></li>
+        <li><a href="badges.html">Components</a></li>
+        <li><a href="collapsible.html">Javascript</a></li>
+        <li><a href="mobile.html">Mobile</a></li>
       </ul>
     </div>
   </nav>
