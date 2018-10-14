59dfd67e5205abd9296f517992dfa7639293c12c
anonimyusdj <anonimyusdj@gmail.com>
2018-10-13 21:03:11 -0600

    menu

--- view/home/menu.php
+++ view/home/menu.php
@@ -1,4 +1,4 @@
-
+<div class="navbar-fixed">
 <nav>
     <div class="nav-wrapper <?php echo $color; ?> darken-3">
       <a href="#!" class="brand-logo"><img src="<?php echo $imagenes;?>logo.jpg" alt=""  width="45px"><?php echo $autor ?></a>
@@ -7,15 +7,16 @@
         <li><a style=" font-size:20px;" href="index.php"><i class="material-icons left">home</i>Inicio</a></li>
         <li><a style=" font-size:20px;" href="sass.html"><i class="material-icons left">panorama</i>Multimedias</a></li>
         <li><a style=" font-size:20px;" href="badges.html"><i class="material-icons left">art_track</i>Eventos</a></li>
-        <li><a style=" font-size:20px;" href="?c=index&a=musica"><i class="material-icons left">headset</i>Música</a></li>
+        <li><a style=" font-size:20px;" href="index.php?c=index&a=musica"><i class="material-icons left">headset</i>Música</a></li>
         <li><a style=" font-size:20px;" href="mobile.html"><i class="material-icons  left">face</i>Biografía</a></li>
-      </ul>
-      <ul class="side-nav" id="mobile-demo">
-        <li><a style=" font-size:20px; font-weight: bold;" href="index.php"><i class="material-icons left">home</i>Inicio</a></li>
-        <li><a style=" font-size:20px; font-weight: bold;" href="sass.html"><i class="material-icons left">panorama</i>Multimedias</a></li>
-        <li><a style=" font-size:20px; font-weight: bold;" href="badges.html"><i class="material-icons left">art_track</i>Eventos</a></li>
-        <li><a style=" font-size:20px; font-weight: bold;" href="?c=index&a=musica"><i class="material-icons left">headset</i>Música</a></li>
-        <li><a style=" font-size:20px; font-weight: bold;" href="mobile.html"><i class="material-icons  left">face</i>Biografía</a></li>
       </ul>
     </div>
   </nav>
+</div>
+  <ul class="side-nav" id="mobile-demo">
+        <li><a style=" font-size:20px; font-weight: bold;" href="index.php"><i class="material-icons left">home</i>Inicio</a></li>
+        <li><a style=" font-size:20px; font-weight: bold;" href="sass.html"><i class="material-icons left">panorama</i>Multimedias</a></li>
+        <li><a style=" font-size:20px; font-weight: bold;" href="badges.html"><i class="material-icons left">art_track</i>Eventos</a></li>
+        <li><a style=" font-size:20px; font-weight: bold;" href="index.php?c=index&a=musica"><i class="material-icons left">headset</i>Música</a></li>
+        <li><a style=" font-size:20px; font-weight: bold;" href="mobile.html"><i class="material-icons  left">face</i>Biografía</a></li>
+      </ul>
