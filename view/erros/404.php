a7c0880fb73fd89be2af58b57e80ff3257aff701
anonimyusdj <anonimyusdj@gmail.com>
2018-10-13 21:24:24 -0600

    error

--- view/erros/404.php
+++ view/erros/404.php
@@ -1,6 +1,6 @@
 <div class="row">
-    <div class="col s12 m3"></div>
-    <div class="col s12 m6">
+    <div class="col s12 m1"></div>
+    <div class="col s12 m10">
         <a class="waves-effect waves-light btn-large <?php echo $color; ?> darken-1"><i class="material-icons right">home</i>Regresar al Home</a>
         <img class="materialboxed responsive-img" width="100%" src="<?php echo $imagenes;?>error4041.png">
         <img class="materialboxed responsive-img" width="100%" src="<?php echo $imagenes;?>error4042.jpg">
