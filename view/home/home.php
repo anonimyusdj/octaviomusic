a0a9d2af3e6b485eaa8032c869afc30fcb9d457f
anonimyusdj <anonimyusdj@gmail.com>
2018-10-15 17:25:50 -0600

    home

--- view/home/home.php
+++ view/home/home.php
@@ -62,7 +62,8 @@
         <br>
         <center>
             <a style=" font-size:20px; font-weight: bold;" href="#!/music" class="red darken-4 waves-effect waves-light btn-large center"> <i class="material-icons medium right">headset</i>Descargar Musica</a>
-        </center><br>
+        <br>
         <div class="chip red darken-4 white-text" style="font-size:12px; font-weight: bold;">Apoyame Descargando mi música gratis o haciendo una donación</div>
+        </center>
     </div>
 </div>
