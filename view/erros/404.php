95182beae23c1b8d20079f28cf830e3a56bcc022
anonimyusdj <anonimyusdj@gmail.com>
2018-10-15 10:27:25 -0600

    img error

--- view/erros/404.php
+++ view/erros/404.php
@@ -1,9 +1,8 @@
-<div class="row">
-    <div class="col s12 m1"></div>
-    <div class="col s12 m10">
-        <a class="waves-effect waves-light btn-large <?php echo $color; ?> darken-1"><i class="material-icons right">home</i>Regresar al Home</a>
-        <img class="materialboxed responsive-img" width="100%" src="<?php echo $imagenes;?>error4041.png">
+<div class="row"><a class="waves-effect waves-light btn-large <?php echo $color; ?> darken-1"><i class="material-icons right">home</i>Regresar al Home</a>
+    <div class="col s12 m6">
+    <img class="materialboxed responsive-img" width="100%" src="<?php echo $imagenes;?>error4041.png">
+    </div>
+    <div class="col s12 m6">
         <img class="materialboxed responsive-img" width="100%" src="<?php echo $imagenes;?>error4042.jpg">
     </div>
-    <div class="col s12 m1"></div>
 </div>
