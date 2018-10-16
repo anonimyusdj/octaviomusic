00be8e7d78e2055a4784abf528444dfa1194415e
anonimyusdj <anonimyusdj@gmail.com>
2018-10-16 00:56:55 -0600

    reproductor

--- view/home/reproductor.php
+++ view/home/reproductor.php
@@ -10,13 +10,12 @@
         <div class="row">
             <div class="col s12 m4">
               <div id="wrapper">
-          <audio preload></audio>
-          <ol>
-        <li><a href="#" data-src="http://kolber.github.io/audiojs/demos/mp3/01-dead-wrong-intro.mp3">dead wrong intro</a></li>
-        <li><a href="#" data-src="http://kolber.github.io/audiojs/demos/mp3/02-juicy-r.mp3">juicy-r</a></li>
-
-      </ol>
-    </div>
+                    <audio preload></audio>
+                    <ol class="collection">
+                        <li class="collection-item"><a href="#" data-src="http://kolber.github.io/audiojs/demos/mp3/01-dead-wrong-intro.mp3">dead wrong intro</a></li>
+                        <li class="collection-item"><a href="#" data-src="http://kolber.github.io/audiojs/demos/mp3/02-juicy-r.mp3">juicy-r</a></li>
+                    </ol>
+              </div>
     <div id="shortcuts">
       <div>
         <h1>Keyboard shortcuts:</h1>
