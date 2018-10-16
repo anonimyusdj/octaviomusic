ce06aed8280ded1c342766a132596adbc885c119
anonimyusdj <anonimyusdj@gmail.com>
2018-10-15 19:03:31 -0600

    reproductor

--- view/home/reproductor.php
+++ view/home/reproductor.php
@@ -7,11 +7,15 @@
     <div class="col s12 m1"></div>
 
     <div class="col s12 m4">  
-
-        <audio controls id="player" controlsList = "nodownload">
-            <img src="<?php echo $imagenes?>album.jpg"  class=" responsive-img" alt="">
-            <source src="<?php echo $musica;?>horse.ogg"  type="audio/ogg">
-        </audio>
+        <div class="row">
+            <div class="col s12 m4">
+                    <img src="<?php echo $imagenes?>album.jpg"  class=" responsive-img" alt="">
+                <audio controls id="player" controlsList = "nodownload">
+                    <source src="<?php echo $musica;?>horse.ogg"  type="audio/ogg">
+                </audio>
+            </div>
+        </div>
+        
         <div class="mask"></div>
     </div>
 
