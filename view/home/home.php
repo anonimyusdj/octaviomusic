e3411517fa96b282aa6151a944138b4b047829e0
anonimyusdj <anonimyusdj@gmail.com>
2018-10-15 20:14:58 -0600

    home

--- view/home/home.php
+++ view/home/home.php
@@ -8,69 +8,69 @@
 </div-->
 <div class="parallax-container">
     <div class="parallax"><img src="<?php echo $imagenes;?>fondo.jpg"></div>
-</div>
-<div class="section white">
-    <div class="row container">
-        <br>
-        <center>
-        <div class="slider">
-            <ul class="slides">
-            <li>
-                <img src="<?php echo $imagenes;?>img1.png"> <!-- random image -->
-            </li>
-            <li>
-                <img src="<?php echo $imagenes;?>img2.jpg"> <!-- random image -->
-            </li>
-            <li>
-                <img src="<?php echo $imagenes;?>img3.jpg"> <!-- random image -->
-            </li>
-            <li>
-                <img src="<?php echo $imagenes;?>img4.jpg"> <!-- random image -->
-            </li>
-            </ul>
-        </div>
-        </center>
-        <div class="row">
-            <div class="col s12 m1"></div>
-            <div class="col s12 m5 card">
-                <header>
-                    <h4>AGRADECIMIENTOS</h4>
-                </header>
-                <hr>
-                <p class="black-text">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estandar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido
-                    usa una galería de textos y los mezcla de tal manera que logro hacer un libro de textos especimen. No solo sobrevivio 500 años, sino que tambien ingreso como texto de relleno en documentos electronicos, quedando esencialmente igual al original.
-                    Fue popularizado en los 60s con la creacion de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y mas recientemente con software de autoedicion, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.</p>
+    <div class="section white">
+        <div class="row container">
+            <br>
+            <center>
+            <div class="slider">
+                <ul class="slides">
+                <li>
+                    <img src="<?php echo $imagenes;?>img1.png"> <!-- random image -->
+                </li>
+                <li>
+                    <img src="<?php echo $imagenes;?>img2.jpg"> <!-- random image -->
+                </li>
+                <li>
+                    <img src="<?php echo $imagenes;?>img3.jpg"> <!-- random image -->
+                </li>
+                <li>
+                    <img src="<?php echo $imagenes;?>img4.jpg"> <!-- random image -->
+                </li>
+                </ul>
             </div>
-            <div class="col s12 m1"></div>
-            <div class="col s12 m4 card">
-                <header>
-                    <h4 class="titulo">SOBRE MI</h4>
-                </header>
-                <hr>
-                <div class="row">
-                    <div class="col s12 md12">
-                        <center><img style="height: 100px;" class="responsive-img circle" src="<?php echo $imagenes;?>avatar.jpg" alt="">
-                            <h5>Octavio Vazquez</h5>
-                        </center>
-                        <hr>
-                    </div>
+            </center>
+            <div class="row">
+                <div class="col s12 m1"></div>
+                <div class="col s12 m5 card">
+                    <header>
+                        <h4>AGRADECIMIENTOS</h4>
+                    </header>
+                    <hr>
+                    <p class="black-text">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estandar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido
+                        usa una galería de textos y los mezcla de tal manera que logro hacer un libro de textos especimen. No solo sobrevivio 500 años, sino que tambien ingreso como texto de relleno en documentos electronicos, quedando esencialmente igual al original.
+                        Fue popularizado en los 60s con la creacion de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y mas recientemente con software de autoedicion, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.</p>
+                </div>
+                <div class="col s12 m1"></div>
+                <div class="col s12 m4 card">
+                    <header>
+                        <h4 class="titulo">SOBRE MI</h4>
+                    </header>
+                    <hr>
+                    <div class="row">
+                        <div class="col s12 md12">
+                            <center><img style="height: 100px;" class="responsive-img circle" src="<?php echo $imagenes;?>avatar.jpg" alt="">
+                                <h5>Octavio Vazquez</h5>
+                            </center>
+                            <hr>
+                        </div>
 
-                    <div class="col s12 m12">
-                        <p>Descripción</p>
+                        <div class="col s12 m12">
+                            <p>Descripción</p>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
 
-        <div class="row">
-            <div class="col s12 m4"></div>
-            <div class="col s12 m5 card">
-                <br>
-                <center>
-                    <a style=" font-size:20px; font-weight: bold;" href="#!/music" class="red darken-4 waves-effect waves-light btn-large center"> <i class="material-icons medium right">headset</i>Descargar Musica</a>
-                <br> <br>
-                <div class="chip red darken-4 white-text" style="font-size:12px; font-weight: bold;">Apoyame Descargando mi música gratis o haciendo una donación</div>
-                </center>
+            <div class="row">
+                <div class="col s12 m4"></div>
+                <div class="col s12 m5 card">
+                    <br>
+                    <center>
+                        <a style=" font-size:20px; font-weight: bold;" href="#!/music" class="red darken-4 waves-effect waves-light btn-large center"> <i class="material-icons medium right">headset</i>Descargar Musica</a>
+                    <br> <br>
+                    <div class="chip red darken-4 white-text" style="font-size:12px; font-weight: bold;">Apoyame Descargando mi música gratis o haciendo una donación</div>
+                    </center>
+                </div>
             </div>
         </div>
     </div>
