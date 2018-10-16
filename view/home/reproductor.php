e2f00d20f710044bbc9eb24c59798f789d4aab0a
anonimyusdj <anonimyusdj@gmail.com>
2018-10-15 19:09:48 -0600

    reproductor

--- view/home/reproductor.php
+++ view/home/reproductor.php
@@ -14,6 +14,18 @@
                     <source src="<?php echo $musica;?>horse.ogg"  type="audio/ogg">
                 </audio>
             </div>
+            <div class="col s12 m4">
+                    <center><img src="<?php echo $imagenes?>album.jpg"  class=" responsive-img" alt=""></center>
+                <audio controls id="player" controlsList = "nodownload">
+                    <source src="<?php echo $musica;?>horse.ogg"  type="audio/ogg">
+                </audio>
+            </div>
+            <div class="col s12 m4">
+                    <center><img src="<?php echo $imagenes?>album.jpg"  class=" responsive-img" alt=""></center>
+                <audio controls id="player" controlsList = "nodownload">
+                    <source src="<?php echo $musica;?>horse.ogg"  type="audio/ogg">
+                </audio>
+            </div>
         </div>
         
         <div class="mask"></div>
