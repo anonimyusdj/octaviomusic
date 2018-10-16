43130e98897259c875f9d2c0d808709ee336854f
anonimyusdj <anonimyusdj@gmail.com>
2018-10-15 19:16:02 -0600

    reproductor

--- view/home/reproductor.php
+++ view/home/reproductor.php
@@ -9,23 +9,11 @@
     <div class="col s12 m4">  
         <div class="row">
             <div class="col s12 m4">
-                    <center><img src="<?php echo $imagenes?>album.jpg"  class=" responsive-img" alt=""></center>
-                <audio controls id="player" controlsList = "nodownload">
+                <audio controls id="player" controlsList = "nodownload" poster="<?php echo $imagenes?>album.jpg">
                     <source src="<?php echo $musica;?>horse.ogg"  type="audio/ogg">
                 </audio>
             </div>
-            <div class="col s12 m4">
-                    <center><img src="<?php echo $imagenes?>album.jpg"  class=" responsive-img" alt=""></center>
-                <audio controls id="player" controlsList = "nodownload">
-                    <source src="<?php echo $musica;?>horse.ogg"  type="audio/ogg">
-                </audio>
-            </div>
-            <div class="col s12 m4">
-                    <center><img src="<?php echo $imagenes?>album.jpg"  class=" responsive-img" alt=""></center>
-                <audio controls id="player" controlsList = "nodownload">
-                    <source src="<?php echo $musica;?>horse.ogg"  type="audio/ogg">
-                </audio>
-            </div>
+            
         </div>
         
         <div class="mask"></div>
