005ab11f34348c573c4b7a8e231a510226543a77
anonimyusdj <anonimyusdj@gmail.com>
2018-10-15 18:29:37 -0600

    reproductor

--- view/home/reproductor.php
+++ view/home/reproductor.php
@@ -6,7 +6,10 @@
     <div class="col s12 m1"></div>
 
     <div class="col s12 m4">
-            
+            <audio src="http://developer.mozilla.org/@api/deki/files/2926/=AudioTest_(1).ogg"
+            autoplay>
+        Your browser does not support the <code>audio</code> element.
+        </audio>    
 
     </div>
 
