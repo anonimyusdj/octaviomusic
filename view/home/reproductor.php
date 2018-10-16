b5b3ced94a80bd050eda3348a20396994d276b7b
anonimyusdj <anonimyusdj@gmail.com>
2018-10-15 18:33:54 -0600

    reproductor

--- view/home/reproductor.php
+++ view/home/reproductor.php
@@ -6,11 +6,15 @@
 
     <div class="col s12 m1"></div>
 
-    <div class="col s12 m4">
-            <audio src="http://developer.mozilla.org/@api/deki/files/2926/=AudioTest_(1).ogg">
-        Your browser does not support the <code>audio</code> element.
-        </audio>    
-
+    <div class="col s12 m4">  
+        <audio id="player" src="http://developer.mozilla.org/@api/deki/files/2926/=AudioTest_(1).ogg"></audio>
+        <div>
+            <button onclick="document.getElementById('player').play()">Play</button>
+            <button onclick="document.getElementById('player').pause()">Pausa</button>
+            <button onclick="document.getElementById('player').load()">Stop</button>
+            <button onclick="document.getElementById('player').volume += .1">Subir volumen</button>
+            <button onclick="document.getElementById('player').volume -= .1">Bajar volumen</button>
+        </div> 
     </div>
 
     <div class="col s12 m1"></div>
