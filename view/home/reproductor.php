61e0ff1e7c8678d83bf767b72bb195679bc93fad
anonimyusdj <anonimyusdj@gmail.com>
2018-10-15 19:01:47 -0600

    reprodutor

--- view/home/reproductor.php
+++ view/home/reproductor.php
@@ -7,8 +7,10 @@
     <div class="col s12 m1"></div>
 
     <div class="col s12 m4">  
+
         <audio controls id="player" controlsList = "nodownload">
-            <source src="<?php echo $music;?>horse.ogg"  type="audio/ogg">
+            <img src="<?php echo $imagenes?>album.jpg"  class=" responsive-img" alt="">
+            <source src="<?php echo $musica;?>horse.ogg"  type="audio/ogg">
         </audio>
         <div class="mask"></div>
     </div>
