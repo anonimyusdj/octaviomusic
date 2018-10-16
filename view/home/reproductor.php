c8ff945fed7b78c84a76ad4c775ca63801a97b08
anonimyusdj <anonimyusdj@gmail.com>
2018-10-15 18:57:56 -0600

    reproductor

--- view/home/reproductor.php
+++ view/home/reproductor.php
@@ -10,13 +10,7 @@
         <audio controls id="player">
             <source src="<?php echo $music;?>horse.ogg"  type="audio/ogg">
         </audio>
-        <div>
-            <button onclick="document.getElementById('player').play()">Play</button>
-            <button onclick="document.getElementById('player').pause()">Pausa</button>
-            <button onclick="document.getElementById('player').load()">Stop</button>
-            <button onclick="document.getElementById('player').volume += .1">Subir volumen</button>
-            <button onclick="document.getElementById('player').volume -= .1">Bajar volumen</button>
-        </div> 
+        <div class="mask"></div>
     </div>
 
     <div class="col s12 m1"></div>
