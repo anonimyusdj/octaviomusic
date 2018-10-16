3f8d199116827b30994b2d94e6bbeebd6747671b
anonimyusdj <anonimyusdj@gmail.com>
2018-10-15 23:24:20 -0600

    patrocinadores

--- view/home/home.php
+++ view/home/home.php
@@ -89,13 +89,13 @@
 <div class="section white">
     <div class="row">
       <h3 class="center">Patrocinadores</h3>
-      <div class="col s12 m4">
+      <div class="col s12 m2">
         <div class="row">
             <div class="col s12 m12">
               <img src="<?php echo $imagenes;?>patrocinador1.jpg" class="responsive-img circle"  width="200px">
             </div>
             <div class="col s12 m12">
-              <p class="promo-caption">Eventos Especiales</p>
+              <p class="promo-caption" style=" font-size: 14px; font-weight: bold;">Eventos Especiales</p>
             </div>
             <div class="col s12 m12">
               Reseña
@@ -103,13 +103,13 @@
         </div>
       </div>
 
-      <div class="col s12 m4">
+      <div class="col s12 m2">
         <div class="row">
             <div class="col s12 m12">
               <img src="<?php echo $imagenes;?>patrocinador21.png" class="responsive-img circle" width="200px">
             </div>
             <div class="col s12 m12">
-              <p class="promo-caption">Innovation Systems</p>
+              <p class="promo-caption" style=" font-size: 14px; font-weight: bold;">Innovation Systems</p>
             </div>
             <div class="col s12 m12">
               Reseña
