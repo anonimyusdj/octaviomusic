b5d5d8dea50b64f00ae539bb17a731ac7e6d90cb
anonimyusdj <anonimyusdj@gmail.com>
2018-10-23 16:29:00 -0600

    reproductor

--- view/home/reproductor.php
+++ view/home/reproductor.php
@@ -1,13 +1,23 @@
 <br><br>
 <div class="row">
-    <div class="col s12 m3">
-          <img src="imagen" onclick="
-      tocata=document.createElement('embed');
-      tocata.src='http://kolber.github.io/audiojs/demos/mp3/01-dead-wrong-intro.mp3';
-      tocata.style.visibility='hidden';
-      tocata.style.position='absolute';
-      document.body.appendChild(tocata);
-      ">
+    <div class="col s12 m3 card">
+      <div class="row">
+   <div class="col s12 m7">
+     <div class="card">
+       <div class="card-image">
+         <img src="images/sample-1.jpg">
+         <span class="card-title">Card Title</span>
+       </div>
+       <div class="card-content">
+         <p>I am a very simple card. I am good at containing small bits of information.
+         I am convenient because I require little markup to use effectively.</p>
+       </div>
+       <div class="card-action">
+         <a href="#">This is a link</a>
+       </div>
+     </div>
+   </div>
+ </div>
 
     </div>
 
