56dc991d2ac2a09e4f855fa8f88b2be212fdd1bf
anonimyusdj <anonimyusdj@gmail.com>
2018-10-23 18:18:15 -0600

    eventos

--- view/home/eventos.php
+++ view/home/eventos.php
@@ -2,15 +2,15 @@
     <div class="col s12 m3 card">
 1
     </div>
-    <div class="col s1 m1"></div>
+    <div class="col s1 m0.5"></div>
     <div class="col s12 m3 card">
 2
     </div>
-    <div class="col s1 m1"></div>
+    <div class="col s1 m0.5"></div>
     <div class="col s12 m3 card">
 3
     </div>
-    <div class="col s1 m1"></div>
+    <div class="col s1 m0.5"></div>
     <div class="col s12 m3 card">
 4
     </div>
