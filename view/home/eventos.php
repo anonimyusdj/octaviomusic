f089a39de1cf108b812dd08cdc5dfb22c86909e3
anonimyusdj <anonimyusdj@gmail.com>
2018-10-23 18:42:35 -0600

    eventos

--- view/home/eventos.php
+++ view/home/eventos.php
@@ -1,5 +1,5 @@
 <div class="row">
-    <div class="col s12 m3 card">
+    <div class="col s12 m3">
 
       <div class="card">
          <div class="card-image waves-effect waves-block waves-light">
@@ -20,17 +20,65 @@
     </div>
 
 
-    <div class="col s1"></div>
-    <div class="col s12 m3 card">
-2
+    <div class="col s12 m1"></div>
+    <div class="col s12 m3">
+
+          <div class="card">
+             <div class="card-image waves-effect waves-block waves-light">
+               <img class="activator" src="<?php echo $imagenes;?>img4.jpg">
+             </div>
+             <div class="card-content">
+               <span class="card-title activator grey-text text-darken-4">Nombre Evento<i class="material-icons right">more_vert</i></span>
+                <h4>Fecha: </h4>
+                <h4>Hora: </h4>
+                <h4>Lugar: </h4>
+             </div>
+             <div class="card-reveal">
+               <span class="card-title grey-text text-darken-4"><i class="material-icons right">close</i>Descripcion</span>
+               <p>Here is some more information about this product that is only revealed once clicked on.</p>
+             </div>
+           </div>
+
     </div>
-    <div class="col s1"></div>
+    <div class="col s12 m1"></div>
     <div class="col s12 m3 card">
-3
+
+      <div class="card">
+         <div class="card-image waves-effect waves-block waves-light">
+           <img class="activator" src="<?php echo $imagenes;?>img4.jpg">
+         </div>
+         <div class="card-content">
+           <span class="card-title activator grey-text text-darken-4">Nombre Evento<i class="material-icons right">more_vert</i></span>
+            <h4>Fecha: </h4>
+            <h4>Hora: </h4>
+            <h4>Lugar: </h4>
+         </div>
+         <div class="card-reveal">
+           <span class="card-title grey-text text-darken-4"><i class="material-icons right">close</i>Descripcion</span>
+           <p>Here is some more information about this product that is only revealed once clicked on.</p>
+         </div>
+       </div>
+
     </div>
-    <div class="col s1"></div>
+    <div class="col s12 m1"></div>
     <div class="col s12 m3 card">
-4
+      
+      <div class="card">
+         <div class="card-image waves-effect waves-block waves-light">
+           <img class="activator" src="<?php echo $imagenes;?>img4.jpg">
+         </div>
+         <div class="card-content">
+           <span class="card-title activator grey-text text-darken-4">Nombre Evento<i class="material-icons right">more_vert</i></span>
+            <h4>Fecha: </h4>
+            <h4>Hora: </h4>
+            <h4>Lugar: </h4>
+         </div>
+         <div class="card-reveal">
+           <span class="card-title grey-text text-darken-4"><i class="material-icons right">close</i>Descripcion</span>
+           <p>Here is some more information about this product that is only revealed once clicked on.</p>
+         </div>
+       </div>
+
     </div>
-    <div class="col s1 m1"></div>
+    <div class="col s12 m1"></div>
 </div>
