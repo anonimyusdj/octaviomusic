2590a591d0e28de43e72dc23d0131a4a28e09b85
anonimyusdj <anonimyusdj@gmail.com>
2018-10-30 18:17:35 -0600

    login

---
+++ view/admin/login.php
@@ -0,0 +1,17 @@
+<div class="row">
+    <form class="col s12">
+      <div class="row">
+        <div class="input-field col s12 m12">
+          <i class="material-icons prefix">email</i>
+          <input id="icon_prefix" type="text" class="validate">
+          <label for="icon_prefix">Usuario</label>
+        </div>
+        <div class="input-field col s12 m12">
+          <i class="material-icons prefix">vpn</i>
+          <input id="icon_telephone" type="password" class="validate">
+          <label for="icon_telephone">Contraseña</label>
+        </div>
+      </div>
+    </form>
+  </div>
+        
