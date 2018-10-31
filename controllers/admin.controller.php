0099aeffd014c277fd8789cc052068888e14f658
anonimyusdj <anonimyusdj@gmail.com>
2018-10-30 18:40:40 -0600

    login

---
+++ controllers/admin.controller.php
@@ -0,0 +1,10 @@
+<?php
+    class AdminController{
+
+      public function login(){
+          require_once "view/partes/header.php";
+          require_once "view/admin/login.php";
+          require_once "view/partes/footer.php";
+      }
+    }
+?>
