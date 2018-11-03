dd0bd0d2b049281dc8179dc0633554c283181adf
Edgar Guzman <anonimyusdj@gmail.com>
2018-11-03 17:51:52 -0600

    Update reproductor.php
    reproductor

--- view/home/reproductor.php
+++ view/home/reproductor.php
@@ -136,7 +136,7 @@
     <div class="row">
       <div class="col s12 m12">
           <header>
-            <h5><i class="material-icons left medium">card_membership</i>Cupones</h5><hr>
+            <h5><i class="material-icons left small>card_membership</i>Cupones</h5><hr>
           </header>
          <a class="btn tooltipped" data-position="bottom" data-tooltip="I am a tooltip">Canjear</a>
       </div>
