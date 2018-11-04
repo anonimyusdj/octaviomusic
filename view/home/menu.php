6d568a7ede11befd991025a26b53974056e00849
Edgar Guzman <anonimyusdj@gmail.com>
2018-11-04 14:45:07 -0600

    Update menu.php
    url menu

--- view/home/menu.php
+++ view/home/menu.php
@@ -4,21 +4,21 @@
       <a href="#!" class="brand-logo"><img src="<?php echo $imagenes;?>logo.jpg" alt=""  width="45px"></a>
       <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
       <ul class="right hide-on-med-and-down">
-        <li><a style=" font-size:20px;" href="index.php"><i class="material-icons left">home</i>Inicio</a></li>
+        <li><a style=" font-size:20px;" href="home"><i class="material-icons left">home</i>Inicio</a></li>
         <li><a style=" font-size:20px;" href="sass.html"><i class="material-icons left">panorama</i>Multimedias</a></li>
-        <li><a style=" font-size:20px;" href="index.php?c=index&a=eventos"><i class="material-icons left">art_track</i>Eventos</a></li>
-        <li><a style=" font-size:20px;" href="index.php?c=index&a=musica"><i class="material-icons left">headset</i>Música</a></li>
+        <li><a style=" font-size:20px;" href="eventos"><i class="material-icons left">art_track</i>Eventos</a></li>
+        <li><a style=" font-size:20px;" href="musica"><i class="material-icons left">headset</i>Música</a></li>
         <li><a style=" font-size:20px;" href="mobile.html"><i class="material-icons  left">face</i>Biografía</a></li>
-        <li><a style=" font-size:20px;" href="index.php?c=index&a=patrocinadores"><i class="material-icons  left">people</i>Patrocinadores</a></li>
+        <li><a style=" font-size:20px;" href="patrocinadores"><i class="material-icons  left">people</i>Patrocinadores</a></li>
       </ul>
     </div>
   </nav>
 </div>
   <ul class="side-nav" id="mobile-demo">
-        <li><a style=" font-size:20px; font-weight: bold;" href="index.php"><i class="material-icons left">home</i>Inicio</a></li>
+        <li><a style=" font-size:20px; font-weight: bold;" href="home"><i class="material-icons left">home</i>Inicio</a></li>
         <li><a style=" font-size:20px; font-weight: bold;" href="sass.html"><i class="material-icons left">panorama</i>Multimedias</a></li>
-        <li><a style=" font-size:20px; font-weight: bold;" href="index.php?c=index&a=eventos"><i class="material-icons left">art_track</i>Eventos</a></li>
-        <li><a style=" font-size:20px; font-weight: bold;" href="index.php?c=index&a=musica"><i class="material-icons left">headset</i>Música</a></li>
+        <li><a style=" font-size:20px; font-weight: bold;" href="eventos"><i class="material-icons left">art_track</i>Eventos</a></li>
+        <li><a style=" font-size:20px; font-weight: bold;" href="musica"><i class="material-icons left">headset</i>Música</a></li>
         <li><a style=" font-size:20px; font-weight: bold;" href="mobile.html"><i class="material-icons  left">face</i>Biografía</a></li>
-        <li><a style=" font-size:20px;" href="index.php?c=index&a=patrocinadores"><i class="material-icons  left">people</i>Patrocinadores</a></li>
+        <li><a style=" font-size:20px; font-weight: bold;" href="patrocinadores"><i class="material-icons  left">people</i>Patrocinadores</a></li>
       </ul>
