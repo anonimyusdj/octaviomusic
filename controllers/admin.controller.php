0e3e26830601beccf22f0d528c611934f4e38dfd
Edgar Guzman <anonimyusdj@gmail.com>
2018-11-04 14:17:38 -0600

    Update admin.controller.php
    login

--- controllers/admin.controller.php
+++ controllers/admin.controller.php
@@ -3,6 +3,7 @@
 
       public function login(){
           require_once "view/partes/header.php";
+          require_once "view/home/menu.php";
           require_once "view/admin/login.php";
           require_once "view/partes/footer.php";
       }
