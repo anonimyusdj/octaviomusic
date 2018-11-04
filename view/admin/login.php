098307988d62e4e384cd3711662a34f45151f950
Edgar Guzman <anonimyusdj@gmail.com>
2018-11-04 15:01:57 -0600

    Update login.php
    color login

--- view/admin/login.php
+++ view/admin/login.php
@@ -8,17 +8,17 @@
                      <h4>Bienvenido</h4>
                      </header></center><hr>
                 <div class="input-field col s12 m12">
-                  <i class="material-icons prefix">email</i>
+                  <i class="material-icons prefix <?php echo $color; ?>-text darken-4">email</i>
                   <input id="icon_prefix" type="text" class="validate">
                   <label for="icon_prefix">Usuario</label>
                 </div>
                 <div class="input-field col s12 m12">
-                  <i class="material-icons prefix">https</i>
+                  <i class="material-icons prefix <?php echo $color; ?>-text darken-4">https</i>
                   <input id="icon_telephone" type="password" class="validate">
                   <label for="icon_telephone">Contraseña</label>
                 </div>
               </div>
-            <center><button class="btn blue darken-3" type="submit">Ingresar</button></center><br>
+            <center><button class="btn <?php echo $color; ?>-text darken-3" type="submit">Ingresar</button></center><br>
             </form>
     </div>
     <div class="col s12 m4">
