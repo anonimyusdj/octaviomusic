7b9d07b97e3b833032258d75d83a99092def37f3
Edgar-Deisy <edgar-deisy@localhost.localdomain>
2018-11-07 18:18:46 -0600

    error db

--- model/db.model.php
+++ model/db.model.php
@@ -3,16 +3,16 @@ require_once "model/config.php";
 
   class Database{
 
-    private $host="35.231.175.105";
-    private $dbname="octaviomusic";
-    private $charset="utf8";
-    private $user="root";
-    private $pass="2016.Deisy";
-
-
     public static function conectar(){
-      $pdo=new PDO("mysql:host=$host;dbname=$dbname;charset=$charset",$user,$pass);
-      $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION,PDO::ERRMODE_WARNING);
+
+          $host="35.231.175.105";
+          $dbname="octaviomusic";
+          $charset="utf8";
+          $user="root";
+          $pass="2016.Deisy";
+
+      $pdo=new PDO("mysql:host=".$host.";dbname=".$dbname.";charset=".$charset."",$user,$pass);
+      $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
       return $pdo;
     }
   }
